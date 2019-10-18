ded9bb7e4c964d2e9da642f9547d5f38e3d1e6cf
Parker Thompson <park.thompson@gmail.com>
2019-10-18 03:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var kittens = ["Milo","Otis","Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  array.shift(name);
-  return array;
+  kittens.shift(name);
+  return kittens;
 }
